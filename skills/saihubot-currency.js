11c2ae5c5fd0a6b51b40051424890f4149f2f3a8
gasolin <gasolin@gmail.com>
2020-10-02 12:25:40 +0800

    use widget to show usd price

--- skills/saihubot-currency.js
+++ skills/saihubot-currency.js
@@ -7,25 +7,34 @@ const BITFINEX_API = 'https://api-pub.bitfinex.com/v2/';
 // https://tw.rter.info/howto_currencyapi.php
 let RTER_API = 'https://tw.rter.info/capi.php';
 
-function fetchUSDtPrice(robot) {
-  fetch(`${PROXY}${BITFINEX_API}ticker/tUSTUSD`)
+function fetchUSDtPrice(robot, rootElement) {
+  return fetch(`${PROXY}${BITFINEX_API}ticker/tUSTUSD`)
   .then(response => response.json())
   .then(json => {
-    const result = `USD / USDt: 現價: ${json[6]} 高: ${json[8]} 低: ${json[9]}`
-    robot.send(result);
-    robot.render();
+    const resultMsg = `USD / USDt: 現價: ${json[6]} 高: ${json[8]} 低: ${json[9]}`
+    const result = document.createTextNode(resultMsg);
+    const br = document.createElement("br");
+    rootElement.appendChild(result);
+    rootElement.appendChild(br);
+    return true;
   })
 }
 
-function fetchUSDPrice(robot) {
-  fetch(`${PROXY}${RTER_API}`)
+function fetchUSDPrice(robot, rootElement) {
+  return fetch(`${PROXY}${RTER_API}`)
   .then(response => response.json())
   .then(json => {
     const usdtwd = `USD / TWD: 現價: ${json.USDTWD.Exrate}`;
-    robot.send(usdtwd);
     const usdsgd = `USD / SGD: 現價: ${json.USDSGD.Exrate}`;
-    robot.send(usdsgd);
-    robot.render();
+    const br = document.createElement("br");
+    const br1 = document.createElement("br");
+    const rennderUsdtwd = document.createTextNode(usdtwd);
+    const rennderUsdsgd = document.createTextNode(usdsgd);
+    rootElement.appendChild(rennderUsdtwd);
+    rootElement.appendChild(br);
+    rootElement.appendChild(rennderUsdsgd);
+    rootElement.appendChild(br1);
+    return true;
   })
 }
 
@@ -35,8 +44,26 @@ SaihuBot.prototype.responses.push({
   name: 'price',
   rule: /forex*|usd price*|美金*|美元*/igs,
   action: function(robot, msg) {
-    robot.send('抓取中...');
-    fetchUSDPrice(robot)
-    fetchUSDtPrice(robot)
+    robot.card({
+      renderLoading: (rootElement) => {
+        const span = document.createElement('span');
+        span.id = robot.getRandomId();
+        const loading = document.createTextNode('抓取中...');
+        const br = document.createElement("br");
+        span.appendChild(loading);
+        span.appendChild(br);
+        rootElement.appendChild(span);
+        return span.id;
+      },
+      asyncAction: (rootElement) => {
+        Promise.race([
+          fetchUSDPrice(robot, rootElement),
+          fetchUSDtPrice(robot, rootElement),
+        ]).then(() => {
+          console.log('clean~')
+          robot.cardIsReady(rootElement);
+        })
+      },
+    })
   },
 });
